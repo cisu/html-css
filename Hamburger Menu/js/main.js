2e477fa787f8b56dbbddbbec47f469497b1aa4cd
cisu <49948070+cisu@users.noreply.github.com>
2020-06-23 14:43:20 +0300

    hamburger menu

---
+++ Hamburger Menu/js/main.js
@@ -0,0 +1,10 @@
+// const navToggle = document.querySelector('.menu-wrap');
+// const navLi = document.querySelectorAll('.nav__link');
+
+
+
+// navLi.forEach(li => {
+//     li.addEventListener('click', () => {
+//         li.parentElement.parentElement.parentElement.parentElement.remove();
+//     })
+// });
